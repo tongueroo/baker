63bbdec80e36b6cac5088b382ea746a769b01e63
Tung Nguyen <tongueroo@gmail.com>
2010-02-02 19:12:50 -0800

    clean up log output

--- lib/run_recipes.rb
+++ lib/run_recipes.rb
@@ -10,6 +10,7 @@ class RunRecipes
   
   def initialize(host, user = nil)
     log "start running chef recipes on #{host}"
+    @debug = false
     @host = host
     @user = user
     Net::SSH.start(@host, @user) do |ssh|
@@ -27,31 +28,33 @@ class RunRecipes
     end
   end
   def upload_chef_configs(ssh)
-    if !File.exist?("config/runchef/dna.json") or !File.exist?("config/runchef/solo.rb")
-      raise "need to create a config/runchef/dna.json and config/runchef/solo.rb file, so it can be uploaded"
+    log "uploading chef configs to #{@host}..."
+    if !File.exist?("config/run_recipes/dna.json") or !File.exist?("config/run_recipes/solo.rb")
+      raise "need to create a config/run_recipes/dna.json and config/run_recipes/solo.rb file, so it can be uploaded to the server that needs it"
     end
-    local_exec("tar czf chef-config.tgz config/runchef")
-    local_exec("scp chef-config.tgz #{@host}:")
-    ssh_exec(ssh, "rm -rf chef-config && tar -zxf chef-config.tgz && mv config/runchef chef-config")
+    bash_exec("tar czf chef-config.tgz config/run_recipes")
+    bash_exec("scp chef-config.tgz #{@host}:")
+    ssh_exec(ssh, "rm -rf chef-config && tar -zxf chef-config.tgz && mv config/run_recipes chef-config")
     ssh_exec(ssh, "rm -f chef-config.tgz")
-    local_exec("rm -f chef-config.tgz")
+    bash_exec("rm -f chef-config.tgz")
   end
   def upload_recipes(ssh)
+    log "uploading chef recipes to #{@host}..."
     @file_cache_path = "/tmp/chef-solo"
     @recipes_path = "/tmp/chef-solo/recipes"
     # create
-    local_exec("tar czf recipes.tgz .")
+    bash_exec("tar czf recipes.tgz .")
     # upload
-    local_exec("scp recipes.tgz #{@host}:")
+    bash_exec("scp recipes.tgz #{@host}:")
     # move
-    log "removing current chef recipes : #{@recipes_path}"
     ssh_exec(ssh, "rm -rf #{@recipes_path}")
-    log "extracting new chef recipes"
     ssh_exec(ssh, "mkdir #{@recipes_path}")
     ssh_exec(ssh, "tar -zxf recipes.tgz -C #{@recipes_path}")
+    bash_exec("rm recipes.tgz")
   end
   
   def run(ssh)
+    log "running chef recipes on #{@host}..."
     ssh_exec(ssh, "chef-solo -c ~/chef-config/solo.rb -j ~/chef-config/dna.json")
   end
   
@@ -59,13 +62,16 @@ private
   def log(msg)
     puts msg
   end
-  def local_exec(command)
-    log `#{command}`
+  def debug(msg)
+    puts msg if @debug
+  end
+  def bash_exec(command)
+    `#{command}`
   end
   def ssh_exec(ssh, command)
 
     unless ARGV.empty?
-      log "Executing command: #{command}"
+      debug "Executing command: #{command}"
     end
 
     stdout = ""
@@ -74,11 +80,9 @@ private
     end
     output = stdout
 
-    # output = ssh.exec!(command)
-
-    if output
+    if output and @debug
       output = output.split("\n").join("\n  ")
-      log "  ssh output: #{output}"
+      debug "  ssh output: #{output}"
     end
 
     output
